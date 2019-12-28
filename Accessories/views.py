43bb1f5df026ca3e8091cce8c72ade87601151d3
Bhanudutta <bhanuparashar23@gmail.com>
2019-12-28 21:32:27 +0530

    Deletion completion

--- Accessories/views.py
+++ Accessories/views.py
@@ -5,13 +5,13 @@ from django.http import JsonResponse
 # Create your views here.
 def rec_del(id):
     childs = ProductTree.objects.filter(parent_id=id)
-    if len(c)>0:
+    if len(childs)>0:
         for c in childs:
             rec_del(c.id);
             c.delete();
     me = ProductTree.objects.get(id=id)
     me.delete();
-    
+
 def index(request):
     return render(request,"main.html")
 def add(request,name,parent_id):
@@ -19,7 +19,8 @@ def add(request,name,parent_id):
     p.save()
     return HttpResponse("Hello added")
 def delete(request,id):
-    rec_del(id);
+    if id!=0:
+        rec_del(id);
     return HttpResponse("deleted")
 def view(request,parent_id):
     ps = ProductTree.objects.filter(parent_id=parent_id)
