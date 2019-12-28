f12b1dc04da747087f9e9b7d06766d11d3e180b6
Bhanudutta <bhanuparashar23@gmail.com>
2019-12-28 20:44:11 +0530

    deletion UI

--- Accessories/views.py
+++ Accessories/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 from Accessories.models import ProductTree
 from django.http import JsonResponse
 # Create your views here.
+def rec_del(id):
+    childs = ProductTree.objects.filter(parent_id=id)
+    if len(c)>0:
+        for c in childs:
+            rec_del(c.id);
+            c.delete();
+    me = ProductTree.objects.get(id=id)
+    me.delete();
+    
 def index(request):
     return render(request,"main.html")
 def add(request,name,parent_id):
@@ -10,8 +19,7 @@ def add(request,name,parent_id):
     p.save()
     return HttpResponse("Hello added")
 def delete(request,id):
-    p = ProductTree.objects.get(id=id)
-    p.delete();
+    rec_del(id);
     return HttpResponse("deleted")
 def view(request,parent_id):
     ps = ProductTree.objects.filter(parent_id=parent_id)
